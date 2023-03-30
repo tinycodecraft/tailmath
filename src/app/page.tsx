02fffa64e782a2fcf2bc1be3ef81851d1863cd05
Raymon Yau <raymond.yau@igsl-group.com>
2023-03-30 17:46:52 +0800

    use MuBulb

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,16 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import MuLink from './ui/MuLink'
 import styles from './page.module.css'
-import { ElCenterGlowSvg, ElCode, ElHeadBanDescription, ElShadowSvg, ElThreeCellWrapper, Eltagparg } from './el/paragraph'
+import {
+  ElCenterGlowSvg,
+  ElCode,
+  ElHeadBanDescription,
+  ElShadowSvg,
+  ElThreeCellWrapper,
+  Eltagparg,
+} from './el/paragraph'
 import { Stack } from '@mui/material'
-
-
-
+import MuBulb from './ui/MuBulb'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,24 +20,25 @@ export default function Home() {
   return (
     <main className='flex flex-col min-h-screen justify-between justify-items-center p-[6rem]'>
       <ElHeadBanDescription>
-        <Eltagparg variant="plight">Get second by editing&nbsp;<ElCode>src/app/page.tsx</ElCode></Eltagparg>
+        <Eltagparg variant='plight'>
+          Get second by editing&nbsp;<ElCode>src/app/page.tsx</ElCode>
+        </Eltagparg>
         <MuLink
-             href='https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-             target='_blank'
-             rel='noopener noreferrer'
-          
-          >
-           By{' '}
-            <Image
-              src='/vercel.svg'
-              alt='Vercel Logo'
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-             
-          </MuLink>
+          className='no-underline text-inherit'
+          href='https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          By{' '}
+          <Image
+            src='/vercel.svg'
+            alt='Vercel Logo'
+            className={styles.vercelLogo}
+            width={100}
+            height={24}
+            priority
+          />
+        </MuLink>
       </ElHeadBanDescription>
       <ElCenterGlowSvg>
         <Image
@@ -46,48 +52,42 @@ export default function Home() {
         <ElShadowSvg variant='blocksquare'>
           <Image src='/thirteen.svg' alt='13' width={40} height={31} priority />
         </ElShadowSvg>
-      </ElCenterGlowSvg>      
+      </ElCenterGlowSvg>
 
       <ElThreeCellWrapper>
-        <a
+        <MuBulb
+          title='Docs'
           href='https://beta.nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
-          className={styles.card}
+          className='no-underline text-inherit'
           target='_blank'
           rel='noopener noreferrer'
         >
-          <h2 className={inter.className}>
-            Docs <span>-&gt;</span>
-          </h2>
           <p className={inter.className}>
             Find in-depth information about Next.js features and API.
           </p>
-        </a>
+        </MuBulb>
 
-        <a
+        <MuBulb
+          title='Templates'
           href='https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
           className={styles.card}
           target='_blank'
           rel='noopener noreferrer'
         >
-          <h2 className={inter.className}>
-            Templates <span>-&gt;</span>
-          </h2>
           <p className={inter.className}>Explore the Next.js 13 playground.</p>
-        </a>
+        </MuBulb>
 
-        <a
+        <MuBulb
+          title='Deploy'
           href='https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app'
           className={styles.card}
           target='_blank'
           rel='noopener noreferrer'
         >
-          <h2 className={inter.className}>
-            Deploy <span>-&gt;</span>
-          </h2>
           <p className={inter.className}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
-        </a>
+        </MuBulb>
       </ElThreeCellWrapper>
     </main>
   )
